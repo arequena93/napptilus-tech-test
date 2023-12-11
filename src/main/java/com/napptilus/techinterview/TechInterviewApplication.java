71407e5f89e99c693da8fa0b9216fbe183614180
antonirequena <antoni.requena@gmail.com>
2023-12-11 16:41:42 +0100

    Minor change to generate the war

--- src/main/java/com/napptilus/techinterview/TechInterviewApplication.java
+++ src/main/java/com/napptilus/techinterview/TechInterviewApplication.java
@@ -2,12 +2,18 @@ package com.napptilus.techinterview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TechInterviewApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TechInterviewApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(TechInterviewApplication.class, args);
     }
 
