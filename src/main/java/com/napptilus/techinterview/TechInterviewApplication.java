a742c0b19cfb5cb141a1837cd63796f2cadea445
antonirequena <antoni.requena@gmail.com>
2023-12-11 12:51:49 +0100

    Preparing the app to be packetized as a war

--- src/main/java/com/napptilus/techinterview/TechInterviewApplication.java
+++ src/main/java/com/napptilus/techinterview/TechInterviewApplication.java
@@ -2,9 +2,10 @@ package com.napptilus.techinterview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TechInterviewApplication {
+public class TechInterviewApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TechInterviewApplication.class, args);
